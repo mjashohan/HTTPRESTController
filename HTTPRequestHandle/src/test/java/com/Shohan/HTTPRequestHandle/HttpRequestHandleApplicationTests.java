264ae26715c681baf7816f4d2bf89cc7be533341
Md Jahan Ali Shohan <mjashohan@gmail.com>
2020-02-22 23:28:28 +0100

    Add files via upload
    This is the server-side design, the client can be any client that can send an HTTP request, like Postman or Telnet or browser.

---
+++ HTTPRequestHandle/src/test/java/com/Shohan/HTTPRequestHandle/HttpRequestHandleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Shohan.HTTPRequestHandle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpRequestHandleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
